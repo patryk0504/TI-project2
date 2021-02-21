15a210aca7a089ff758bbf77d45271f96f8da020
patryk0504 <ppatryk.sledz@gmail.com>
2021-02-21 17:51:07 +0100

    porting site to ms azure

--- rest/mongo.php
+++ rest/mongo.php
@@ -7,12 +7,19 @@ require "../vendor/autoload.php";
 class db {
     private $user = "8sledzp" ;
     private $pass = "pass8sledzp";
-    private $host = "172.20.44.25";
-    private $base = "8sledzp";
+    // private $host = "172.20.44.25";
+    private $base = "movie_survey";
     public $conn;
  
     public function __construct() {
-      $connClient = new MongoDB\Client("mongodb://{$this->user}:{$this->pass}@{$this->host}/{$this->base}"); 
+      // $connClient = new MongoDB\Client("mongodb://{$this->user}:{$this->pass}@{$this->host}/{$this->base}"); 
+      
+      $connClient = new MongoDB\Client("mongodb+srv://{$this->user}:{$this->pass}@cluster0.fs0hf.mongodb.net/{$this->base}?retryWrites=true&w=majority"); 
+
+      // $connClient = new MongoDB\Client('mongodb+srv://patryk0504:2612Oralb@cluster0.fs0hf.mongodb.net/movie_survey?retryWrites=true&w=majority');
+      // $dbs = $connClient->listDatabases();
       $this->conn = $connClient -> {$this->base};   
     }
 }
+
+// $test = new db();
